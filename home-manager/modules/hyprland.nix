331ec63580ebc5d889bbccbafa8c360771e1b2d0
lcdse7en <2353442022@qq.com>
2024-07-01 22:17:04 +0800

    add

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -1,15 +1,29 @@
 { config, pkgs, configDir, ... }:
 {
-  # wayland.windowManager.hyprland = {
-  #   enable = true;
-  #   package = pkgs.hyprland;
-  #   xwayland.enable = true;
-  #   systemd.enable = true;
-  # };
-  programs.hyprland = {
+  wayland.windowManager.hyprland = {
     enable = true;
+    # package = pkgs.hyprland;
     xwayland.enable = true;
+    systemd.enable = true;
   };
+
+  settings = {
+    source = [
+      "~/.config/hypr/hyprland.conf"
+    ];
+  };
+  "$mainMod" = "SUPER"; # windows key
+  "$terminal" = "wezterm";
+  "$browser" = "brave";
+
+  bind = [
+    "$mainMod SHIFT, Q, exec, exit"
+    "$mainMod, Return, exec, $terminal"
+  ];
+  # programs.hyprland = {
+  #   enable = true;
+  #   xwayland.enable = true;
+  # };
 
   # home.packages = with pkgs; [
   #   swww
