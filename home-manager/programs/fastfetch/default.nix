1d2e8a7701151561cfaa23b9944eeb1bb071a5ab
lcdse7en <2353442022@qq.com>
2024-07-04 23:49:44 +0800

    add

--- home-manager/programs/fastfetch/default.nix
+++ home-manager/programs/fastfetch/default.nix
@@ -5,161 +5,96 @@
   };
 
   xdg.configFile."fastfetch/config.jsonc".text = ''
-    {
-      "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
-      "logo": {
-        # "source": "goku.png",
-        "type": "iterm",
-        "height": 20,
-        "width": 40,
-        "padding": {
-          "top": 5,
+      {
+        "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+          "logo": {
+              "source": "~/.config/fastfetch/planet",
+              "color": {
+                  "1": "blue",
+                  "2": "magenta"
+              },
+              "type": "file",
+              "padding": {
+                  "top": 0,
+                  "left": 0
+              }
+          },
+        "display": {
+          "separator": "    "
         },
-      },
-      "display": {
-        "separator": " -> ",
-      },
-      "modules": [
-        {
-          "type": "custom",
-          "format": "\u001b[90m┌────────────────────────────────────────────────────────────┐",
-        },
-        {
-          "type": "title",
-          "keyWidth": 10,
-        },
-        {
-          "type": "custom",
-          "format": "\u001b[90m└────────────────────────────────────────────────────────────┘",
-        },
-        {
-          "type": "custom",
-          "format": " \u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m  \u001b[38m  \u001b[39m       \u001b[38m  \u001b[37m  \u001b[36m  \u001b[35m  \u001b[34m  \u001b[33m  \u001b[32m  \u001b[31m  \u001b[90m",
-        },
-        {
-          "type": "custom",
-          "format": "\u001b[90m┌────────────────────────────────────────────────────────────┐",
-        },
-        {
-          "type": "os",
-          "key": " OS",
-          "keyColor": "yellow",
-        },
-        {
-          "type": "kernel",
-          "key": "│ ├",
-          "keyColor": "yellow",
-        },
-        {
-          "type": "packages",
-          "key": "│ ├󰏖",
-          "keyColor": "yellow",
-        },
-        {
-          "type": "shell",
-          "key": "│ └",
-          "keyColor": "yellow",
-        },
-        {
-          "type": "de",
-          "key": " DE",
-          "keyColor": "blue",
-        },
-        {
-          "type": "wm",
-          "key": "│ ├",
-          "keyColor": "blue",
-        },
-        {
-          "type": "lm",
-          "key": "│ ├󰧨",
-          "keyColor": "blue",
-        },
-        {
-          "type": "wmtheme",
-          "key": "│ ├󰉼",
-          "keyColor": "blue",
-        },
-        {
-          "type": "icons",
-          "key": "│ ├󰀻",
-          "keyColor": "blue",
+        "modules": [
+          {
+            "type": "custom",
+            "format": "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+          },
+          "break",
+          {
+            "type": "os",
+            "key": "  ",
+            "keyColor": "blue"
+          },
+          {
+            "type": "kernel",
+            "key": "  ",
+            "keyColor": "white"
+          },
+          {
+            "type": "packages",
+            "key": "  󰮯",
+            "keyColor": "yellow"
+          },
+          {
+            "type": "wm",
+            "key": "  󰨇",
+            "keyColor": "blue"
         },
         {
           "type": "terminal",
-          "key": "│ ├",
-          "keyColor": "blue",
+          "key": "  ",
+          "keyColor": "magenta"
         },
         {
-          "type": "wallpaper",
-          "key": "│ └󰸉",
-          "keyColor": "blue",
-        },
-        {
-          "type": "host",
-          "key": " PC",
-          "keyColor": "green",
-        },
-        {
-          "type": "cpu",
-          "key": "│ ├",
-          "keyColor": "green",
-        },
-        {
-          "type": "gpu",
-          "key": "│ ├󰍛",
-          "keyColor": "green",
-        },
-        {
-          "type": "disk",
-          "key": "│ ├",
-          "keyColor": "green",
-        },
-        {
-          "type": "memory",
-          "key": "│ ├󰑭",
-          "keyColor": "green",
-        },
-        {
-          "type": "swap",
-          "key": "│ ├󰓡",
-          "keyColor": "green",
-        },
-        {
-          "type": "uptime",
-          "key": "│ ├󰅐",
-          "keyColor": "green",
-        },
-        {
-          "type": "display",
-          "key": "│ └󰍹",
-          "keyColor": "green",
-        },
-        {
-          "type": "sound",
-          "key": " SND",
-          "keyColor": "cyan",
-        },
-        {
-          "type": "player",
-          "key": "│ ├󰥠",
-          "keyColor": "cyan",
-        },
-        {
-          "type": "media",
-          "key": "│ └󰝚",
-          "keyColor": "cyan",
-        },
-        {
-          "type": "custom",
-          "format": "\u001b[90m└────────────────────────────────────────────────────────────┘",
+          "type": "shell",
+          "key": "  ",
+          "keyColor": "yellow"
         },
         "break",
         {
           "type": "custom",
-          "format": " \u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m  \u001b[38m  \u001b[39m       \u001b[38m  \u001b[37m  \u001b[36m  \u001b[35m  \u001b[34m  \u001b[33m  \u001b[32m  \u001b[31m  \u001b[90m",
+          "format": "┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
         },
-      ],
+        "break",
+        {
+          "type": "host",
+          "key": "  ",
+          "keyColor": "bright_blue"
+        },
+        {
+          "type": "cpu",
+          "key": "  ",
+          "keyColor": "bright_green"
+        },
+        {
+          "type": "gpu",
+          "key": "  󱤓",
+          "keyColor": "red"
+        },
+        {
+          "type": "memory",
+          "key": "  󰍛",
+          "keyColor": "bright_yellow"
+        },
+        {
+          "type": "disk",
+          "key": "  ",
+          "keyColor": "bright_cyan"
+        },
+        "break",
+        {
+          "type": "custom",
+          "format": "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+        }
+      ]
     }
   '';
 
