87969c4744916ecf8714e1db8d320237292ad871
lcdse7en <2353442022@qq.com>
2024-07-04 15:02:20 +0000

    add

--- home-manager/programs/swaylock/default.nix
+++ home-manager/programs/swaylock/default.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 {
-  programs = {
-    swaylock.enable = true;
+  # programs = {
+  #   swaylock.enable = true;
+  # };
+
+  home = {
+    packages = with pkgs; [
+      swaylock
+    ];
   };
   home.file = {
     ".config/swaylock/config".source = ./config;
