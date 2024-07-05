54cb96a9598fd96501177011c275b4193d05304c
lcdse7en <2353442022@qq.com>
2024-07-05 23:03:27 +0800

    add

--- flake.nix
+++ flake.nix
@@ -40,7 +40,8 @@
     flake-root.url = "github:srid/flake-root";
 
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.41.2";
+      # url = "github:hyprwm/Hyprland?ref=v0.41.2";
+      url = "github:hyprwm/Hyprland?ref=v0.40.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hycov = {
