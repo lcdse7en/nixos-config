a61e359be50559d06ada42029d119ccaeadb3c21
lcdse7en <2353442022@qq.com>
2024-07-05 17:09:27 +0800

    add

--- home-manager/wm/hyprland/config.nix
+++ home-manager/wm/hyprland/config.nix
@@ -360,8 +360,10 @@
       windowrulev2 = opacity 0.90 0.90,class:^(firefox)$
       windowrulev2 = opacity 0.90 0.90,class:^(Brave-browser)$
       windowrulev2 = opacity 0.80 0.80,class:^(kitty)$
+      windowrulev2 = opacity 0.80 0.80,class:^(wezterm)$
 
       windowrule=opacity 0.8,kitty
+      windowrule=opacity 0.8,wezterm
       windowrule=float,title:^(Picture-in-Picture)$
       windowrule=size 960 540,title:^(Picture-in-Picture)$
       windowrule=move 25%-,title:^(Picture-in-Picture)$
