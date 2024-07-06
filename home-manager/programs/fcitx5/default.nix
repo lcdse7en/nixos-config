58b9d2ce10c4e8eaffd6128e86914f33f1a1150a
lcdse7en <2353442022@qq.com>
2024-07-06 23:47:05 +0800

    add

--- home-manager/programs/fcitx5/default.nix
+++ home-manager/programs/fcitx5/default.nix
@@ -1,8 +1,24 @@
-{ pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons fcitx5-table-extra fcitx5-pinyin-moegirl fcitx5-pinyin-zhwiki fcitx5-mozc ];
+    fcitx5.addons = with pkgs; [
+      rime-data
+      fcitx5-rime
+      fcitx5-chinese-addons
+      fcitx5-table-extra
+      fcitx5-pinyin-moegirl
+      fcitx5-pinyin-zhwiki
+      fcitx5-mozc
+      (fcitx5-rime.override {
+        rimeDataPkgs = with config.nur.repos.xddxdd; [
+          rime-data
+          rime-aurora-pinyin
+          rime-dict
+          rime-moegirl
+          rime-zhwiki
+        ];
+      })
+    ];
   };
   home.file = {
     ".config/fcitx5/conf/classicui.conf".source = ./classicui.conf;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,6 +14,7 @@
     ./fonts.nix
     ./desktop.nix
     ./udiskie.nix
+    ./fcitx5.nix
     # ./impermanence.nix
   ];
 
