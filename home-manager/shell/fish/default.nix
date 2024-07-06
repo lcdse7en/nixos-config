af4b2ccb050ecf5f43c0f58d212af9c527100289
lcdse7en <2353442022@qq.com>
2024-07-06 19:47:15 +0800

    add

--- home-manager/shell/fish/default.nix
+++ home-manager/shell/fish/default.nix
@@ -8,6 +8,7 @@ let
     "e" = "exit";
     "s" = "fastfetch";
     "top" = "btop";
+    "gc" = "git clone";
   };
   aliases = {
     "rebuild" = "sudo nixos-rebuild switch --flake ${flakePath}";
@@ -18,25 +19,21 @@ let
     ll = "ls -l --git";
     l = "ls -laH";
     lg = "ls -lG";
-    oplocal =
-      "./js/oph/dist/mac-arm64/1Password.app/Contents/MacOS/1Password";
+    oplocal = "./js/oph/dist/mac-arm64/1Password.app/Contents/MacOS/1Password";
     # inspect $PATH
     pinspect = ''echo "$PATH" | tr ":" "\n"'';
   };
-in
-{
+in {
   programs.fish = {
     enable = true;
-    loginShellInit =
-      if config.wayland.windowManager.hyprland.enable then ''
-        set TTY1 (tty)
-        [ "$TTY1" = "/dev/tty1" ] && exec Hyprland
-      ''
-      else if config.wayland.windowManager.sway.enable then ''
-        set TTY1 (tty)
-        [ "$TTY1" = "/dev/tty1" ] && exec sway
-      ''
-      else '''';
+    loginShellInit = if config.wayland.windowManager.hyprland.enable then ''
+      set TTY1 (tty)
+      [ "$TTY1" = "/dev/tty1" ] && exec Hyprland
+    '' else if config.wayland.windowManager.sway.enable then ''
+      set TTY1 (tty)
+      [ "$TTY1" = "/dev/tty1" ] && exec sway
+    '' else
+      "";
     interactiveShellInit = ''
       set fish_greeting ""
       set fish_key_bindings  fish_vi_key_bindings
@@ -53,7 +50,8 @@ in
   };
   home.file = {
     ".config/fish/conf.d/nord.fish".text = import ./nord_theme.nix;
-    ".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
+    ".config/fish/functions/fish_prompt.fish".source =
+      ./functions/fish_prompt.fish;
     ".config/fish/functions/xdg-get.fish".text = import ./functions/xdg-get.nix;
     ".config/fish/functions/xdg-set.fish".text = import ./functions/xdg-set.nix;
     ".config/fish/functions/owf.fish".text = import ./functions/owf.nix;
