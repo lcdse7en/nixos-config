6c92037d35dba673654b0710a7be99dccddc0082
lcdse7en <2353442022@qq.com>
2024-07-06 16:03:06 +0800

    add

--- home-manager/programs/obs-studio/default.nix
+++ home-manager/programs/obs-studio/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 let
   obsThemes = pkgs.fetchFromGitHub {
     owner = "catppuccin";
@@ -6,10 +6,17 @@ let
     rev = "9a78d89";
     sha256 = "sha256-8DjAjpYsC9lEHe6gt/B7YCyfqVPaA5Qg1hbIMyyx/ho=";
   };
+  customSystem = inputs.nixpkgs-system.legacyPackages.${pkgs.system};
 in
 {
-  programs = {
-    obs-studio.enable = true;
+  programs.obs-studio = {
+    enable = true;
+    plugins = with customSystem.obs-studio-plugins; [
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vkcapture
+      wlrobs
+    ];
   };
   home.file = {
     ".config/obs-studio/themes".source = "${obsThemes}/themes";
