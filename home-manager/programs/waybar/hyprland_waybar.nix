89030b0bb3dc64615b4cd9783ecd6229e50f53d8
lcdse7en <2353442022@qq.com>
2024-07-06 20:24:18 +0800

    add

--- home-manager/programs/waybar/hyprland_waybar.nix
+++ home-manager/programs/waybar/hyprland_waybar.nix
@@ -1,14 +1,14 @@
 { ... }: {
   programs.waybar = {
     enable = true;
-    catppuccin.enable = false;
+    # catppuccin.enable = false;
     style = ./style.css;
     settings = {
       mainBar = {
         margin-top = 10;
         margin-left = 7;
         width = 1900;
-        height = 32;
+        height = 22;
         modules-left = [ "hyprland/workspaces" ];
         modules-center = [ "clock" ];
         modules-right = [ "network" "memory" "battery" "pulseaudio" ];
