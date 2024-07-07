23ce49ac23d5b3c777ffbae75b21249bec264770
lcdse7en <2353442022@qq.com>
2024-07-08 00:58:27 +0800

    add

--- home-manager/programs/mako/default.nix
+++ home-manager/programs/mako/default.nix
@@ -1,8 +1,16 @@
-{...}: {
+{ ... }: {
   services.mako = {
     enable = true;
-    font = "CaskaydiaCove Nerd Font 12";
+    font = "Hack Nerd Font Mono 10";
     defaultTimeout = 5000;
     ignoreTimeout = true;
+    border-radius = 8;
+    text-color = "#2e3440ff";
+    background-color = "#eceff4f4";
+    border-color = "#d8dee9ff";
+    border-size = 0;
+    margin = [ 12 12 6 ];
+    padding = [ 12 12 12 12 ];
+    max-visible = 3;
   };
 }
