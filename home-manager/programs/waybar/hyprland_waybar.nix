a0bc1e9915dab62666ba68431f63e0397b140569
lcdse7en <2353442022@qq.com>
2024-07-07 20:26:23 +0800

    add

--- home-manager/programs/waybar/hyprland_waybar.nix
+++ home-manager/programs/waybar/hyprland_waybar.nix
@@ -1,30 +1,131 @@
-{ ... }: {
+{ config, pkgs, ... }: {
   home.packages = with pkgs; [
     font-awesome_6
     (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
   ];
 
-  programs.waybar = {
-    enable = true;
-    # catppuccin.enable = false;
-    style = ./style.css;
-    settings = {
-      mainBar = {
-        margin-top = 10;
-        margin-left = 7;
-        width = 1900;
-        height = 22;
-        modules-left = [ "hyprland/workspaces" ];
-        modules-center = [ "clock" ];
-        modules-right = [ "network" "memory" "battery" "pulseaudio" ];
-        clock = {
-          format = " {:%r  %d/%m/%y}";
-          interval = 1;
-          tooltip-format = ''
-            <big>{:%Y %B}</big>
-            <tt><small>{calendar}</small></tt>'';
+  # programs.waybar = {
+  #   enable = true;
+  #   # catppuccin.enable = false;
+  #   style = ./style.css;
+  #   settings = {
+  #     mainBar = {
+  #       margin-top = 10;
+  #       margin-left = 7;
+  #       width = 1900;
+  #       height = 22;
+  #       modules-left = [ "hyprland/workspaces" ];
+  #       modules-center = [ "clock" ];
+  #       modules-right = [ "network" "memory" "battery" "pulseaudio" ];
+  #       clock = {
+  #         format = " {:%r  %d/%m/%y}";
+  #         interval = 1;
+  #         tooltip-format = ''
+  #           <big>{:%Y %B}</big>
+  #           <tt><small>{calendar}</small></tt>'';
+  #       };
+  #     };
+  #   };
+  # };
+
+  programs.waybar.enable = true;
+
+  programs.waybar.settings = {
+    mainBar = {
+      layer = "top";
+      position = "top";
+      height = 26;
+      output = [ "eDP-1" ];
+
+      modules-left = [ "custom/logo" "clock" "bluetooth" ];
+      modules-right = [ "cpu" "network" "battery" ];
+      modules-center = [ "hyprland/workspaces" ];
+
+      "bluetooth" = {
+        format = "   {status}";
+        format-disabled = "";
+        format-connected = "   {num_connections}";
+      };
+
+      "custom/logo" = {
+        format = "";
+        tooltip = false;
+      };
+
+      "hyprland/workspaces" = {
+        format = "{icon}";
+        tooltip = false;
+        all-outputs = true;
+        format-icons = {
+          active = "";
+          default = "";
         };
+      };
+
+      "memory" = {
+        interval = "2";
+        format = "RAM: {usage}GiB |";
+      };
+
+      "cpu" = {
+        interval = "1";
+        format = "❤️ {max_frequency}GHz";
+        max-length = 13;
+        min-length = 13;
+        # on-click = "alacritty -e htop --sort-key PERCENT_CPU";
+        tooltip = false;
+      };
+
+      "network" = {
+        format = "  ";
+        format-ethernet = "  ";
+        tooltip = false;
+      };
+
+      "battery" = {
+        format = "{icon}   {capacity}%";
+        format-charging = "⚡{capacity}%";
+        format-icons = [ "" "" "" "" "" ];
+        states = { critical = 20; };
+        tooltip = false;
       };
     };
   };
+
+  programs.waybar.style = ''
+    * {
+      border: none;
+      border-radius: 0;
+      padding: 0;
+      margin: 0;
+      font-size: 12px;
+      color: #b4befe
+    }
+
+    window#waybar {
+      background: #11111b;
+      color: #ffffff;
+    }
+
+    #custom-logo {
+      font-size: 18px;
+      margin: 0;
+      margin-left: 7px;
+      margin-right: 12px;
+      padding: 0;
+      font-family: Fira Code;
+      color: #b4befe;
+    }
+
+    #workspaces button {
+      color: #b4befe;
+    }
+
+    #battery {
+      margin-left: 7px;
+      margin-right: 5px;
+      color: #b4befe;
+    }
+  '';
+
 }
