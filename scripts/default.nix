f0d3caa2974f79d59b66d508371c52d0f907c238
lcdse7en <2353442022@qq.com>
2024-07-09 00:17:09 +0800

    add

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,14 +1,9 @@
 let
   scripts = { config, lib, pkgs, ... }:
-    let
-      gen-ssh-key = pkgs.callPackage ./gen-ssh-key.nix { inherit pkgs; };
-      kls = pkgs.callPackage ./keyboard-layout-switch.nix { inherit pkgs; };
-      szp = pkgs.callPackage ./show-zombie-parents.nix { inherit pkgs; };
+    let gen-ssh-key = pkgs.callPackage ./gen-ssh-key.nix { inherit pkgs; };
     in {
       home.packages = [
         gen-ssh-key # generate ssh key and add it to the system
-        kls # switch keyboard layout
-        szp # show zombie parents
       ] ++ (pkgs.sxm.scripts or [ ]);
     };
 in [ scripts ]
