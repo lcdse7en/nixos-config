738b260546984eaa2fe4470777c764b1d8cc535c
lcdse7en <2353442022@qq.com>
2024-07-09 19:56:17 +0800

    add

--- home-manager/programs/waybar/hyprland_waybar.nix
+++ home-manager/programs/waybar/hyprland_waybar.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 let sharedScripts = import ./share_scripts.nix { inherit pkgs; };
 in {
   home.packages = with pkgs; [ wttrbar wl-screenrec jq zenity wlogout ];
@@ -17,7 +17,8 @@ in {
         "memory"
         "temperature"
         "custom/filler"
-        "custom/playerctl"
+        # "custom/playerctl"
+        "custom/spotify"
         "custom/filler"
         "disk"
         "custom/recorder"
@@ -36,6 +37,22 @@ in {
         "custom/power"
         # "custom/reboot"
       ];
+      "custom/spotify" = {
+        format = "{icon} {}";
+        return-type = "json";
+        max-length = 40;
+        escape = true;
+        on-click = "${lib.getExe pkgs.playerctl} -p spotify play-pause";
+        on-click-right = "killall spotify";
+        smooth-scrolling-threshold = 10;
+        on-scroll-up = "${lib.getExe pkgs.playerctl} -p spotify next";
+        on-scroll-down = "${lib.getExe pkgs.playerctl} -p spotify previous";
+        exec = "${
+            lib.getExe (pkgs.callPackage ./mediaplayer.nix { })
+          } --player spotify 2> /dev/null";
+        exec-if = "pgrep spotify";
+        restart-interval = 2;
+      };
     }];
     style = builtins.readFile ./style.css;
     # systemd.enable = true;
