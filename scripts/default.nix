970badc24bf453c107cf7bdf7dd7c4d7be74c4bb
lcdse7en <2353442022@qq.com>
2024-07-09 17:06:41 +0800

    add

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,6 +1,8 @@
 let
   scripts = { config, lib, pkgs, ... }:
-    let gen-ssh-key = pkgs.callPackage ./gen-ssh-key.nix { inherit pkgs; };
+    let
+      gen-ssh-key = pkgs.callPackage ./gen-ssh-key.nix { inherit pkgs; };
+      recorder = pkgs.callPackage ./recorder.nix { inherit pkgs; };
     in {
       home.packages = [
         gen-ssh-key # generate ssh key and add it to the system
