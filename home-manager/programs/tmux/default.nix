1c9af0ce511e6f35efdda9dd26e01e122b9a0dbe
lcdse7en <2353442022@qq.com>
2024-07-13 18:27:58 +0800

    add

--- home-manager/programs/tmux/default.nix
+++ home-manager/programs/tmux/default.nix
@@ -11,22 +11,22 @@ in {
     extraConfig = tmuxConf;
     escapeTime = 0;
     keyMode = "vi";
-    plugins = with plugins; [
-      cpu
-      nord # theme
-      {
-        plugin = resurrect;
-        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-      }
-      {
-        plugin = continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '60' # minutes
-        '';
-      }
-    ];
-    shortcut = "a";
-    terminal = "screen-256color";
+    #   plugins = with plugins; [
+    #     cpu
+    #     nord # theme
+    #     {
+    #       plugin = resurrect;
+    #       extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+    #     }
+    #     {
+    #       plugin = continuum;
+    #       extraConfig = ''
+    #         set -g @continuum-restore 'on'
+    #         set -g @continuum-save-interval '60' # minutes
+    #       '';
+    #     }
+    #   ];
+    #   shortcut = "a";
+    #   terminal = "screen-256color";
   };
 }
